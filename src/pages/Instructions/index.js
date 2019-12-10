7b9d092486c61e87c200bff88bb4068760df7140
noxormy <noxormy@gmail.com>
2019-12-10 12:36:34 +0300

    partial fix problem with copied sign

--- src/pages/Instructions/index.js
+++ src/pages/Instructions/index.js
@@ -6,31 +6,39 @@ function Index(props) {
     let className = 'instructions';
     let classNameAppear = className + ' appear';
 
-    const [instructionsClass, setInstructionsClass] = useState(className);
+    const {apiKey} = props;
 
+    const [instructionsClass, setInstructionsClass] = useState(className);
     const [installCopied, setInstallCopied] = useState('');
+    const [apiKeyCopied, setApiKeyCopied] = useState('');
     const [usageCopied, setUsageCopied] = useState('');
 
     useEffect(() => {
         if (instructionsClass === className) setInstructionsClass(classNameAppear);
-        if (installCopied) setTimeout(() => setInstallCopied(''), 2000);
-        if (usageCopied) setTimeout(() => setUsageCopied(''), 2000);
+        if (installCopied) delay(setInstallCopied);
+        if (apiKeyCopied) delay(setApiKeyCopied);
+        if (usageCopied) delay(setUsageCopied);
     });
     return (
         <div className={instructionsClass}>
             <div className={'install ' + installCopied}>
-                <div onClick={() => copy(install, setInstallCopied)}>{copyIcon}</div>
+                <div onClick={() => setter(install, installCopied, setInstallCopied)}>{copyIcon}</div>
                 $ {install}
             </div>
+            <h3 className='label'>Your api key:</h3>
+            <div className={'apiKey ' + apiKeyCopied}>
+                <div onClick={() => setter(apiKey, apiKeyCopied, setApiKeyCopied)}>{copyIcon}</div>
+                {apiKey}
+            </div>
             <div className={'usage ' + usageCopied}>
-                <div onClick={() => copy(usage, setUsageCopied)}>{copyIcon}</div>
-                {getUsageBlock(props.apiKey)}
+                <div onClick={() => setter(usage, usageCopied, setUsageCopied)}>{copyIcon}</div>
+                {getUsageBlock(apiKey)}
             </div>
         </div>
     );
 }
 
-function copy(text, setCopied) {
+function copy(text, setCopied, copiedClass) {
     let textarea = document.createElement('textarea');
     textarea.setAttribute('visibility', 'collapse');
     textarea.textContent = text;
@@ -39,9 +47,20 @@ function copy(text, setCopied) {
     document.execCommand('copy');
     textarea.remove();
 
-    let copiedClass = 'copied';
     setCopied(copiedClass);
     console.log(copiedClass);
+}
+
+function setter(valueToCopy, variable, callback) {
+    const copiedClass = 'copied';
+    if (!variable.includes(copiedClass))
+        copy(valueToCopy, callback, copiedClass)
+}
+
+function delay(callback, delay = 2000) {
+    setTimeout(() => {
+        callback('');
+    }, delay)
 }
 
 export default Index;

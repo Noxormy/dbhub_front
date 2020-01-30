ef8cb09d9fb882f5ae1ae6c26f3cf9e06396676d
noxormy <noxormy@gmail.com>
2020-01-30 04:39:15 +0300

    Updates

---
+++ precache-manifest.b8efc3c5382ac8b6a5390c3ec3beec79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef7bfccc7bc8a571cf954d2146b8a82",
+    "url": "/dbhub_front/index.html"
+  },
+  {
+    "revision": "3543a3ecae6797cec678",
+    "url": "/dbhub_front/static/css/main.3e6b4d25.chunk.css"
+  },
+  {
+    "revision": "53b6540b1321aa9f4d7d",
+    "url": "/dbhub_front/static/js/2.61abaa71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dbhub_front/static/js/2.61abaa71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3543a3ecae6797cec678",
+    "url": "/dbhub_front/static/js/main.d245088e.chunk.js"
+  },
+  {
+    "revision": "ed6fa2026c41154ccacb",
+    "url": "/dbhub_front/static/js/runtime-main.2f78d774.js"
+  }
+]);

fc75cfb83753a26dd8d5b7d5e926622d060fbb98
noxormy <noxormy@gmail.com>
2021-01-17 16:39:58 +0300

    update styles

--- src/pages/Instructions/codeExample.js
+++ src/pages/Instructions/codeExample.js
@@ -1,91 +1,50 @@
-import React from "react"
-import './codeExample.sass'
-
-export const NODE_JS = 'nodejs'
+export const JAVASCRIPT = 'javascript'
 export const PYTHON = 'python'
 
-export const getUsageBlock = (language, apiKey) => {
-    const languageUsages = {
-        'nodejs': (
-            <p>
-                <span className="keyword">const</span> {'{'}<span className="variable">getDatabase</span>{'}'} =<span className="func"> require</span>(<span className="string">'dbhub'</span>)<br/>
-                <br/>
-                <span className="keyword">let</span> <span className="variable">apiKey</span> =<span className="string"> '{apiKey}'</span><br/>
-                <span className="keyword">let</span> <span className="variable">db</span> =<span className="func"> getDatabase</span>(<span className="variable">apiKey</span>)<br/>
-                <br/>
-                {'// Create collection'}<br/>
-                <span className="keyword">let</span> <span className="variable">collectionName</span> = <span className="string">'example'</span><br/>
-                <span className="keyword">let</span> <span className="variable">collection</span> = <span className="variable">db</span>.<span className="func">getCollection</span>(<span className="variable">collectionName</span>)<br/>
-                <br/>
-                {'//Usage'}<br/>
-                <span className="variable">collection</span>.<span className="func">create</span>(<span className="variable">element</span>).<span className="func">then</span>((<span className="variable">id</span>) => {'{}'})<br/>
-                <span className="variable">collection</span>.<span className="func">read</span>(<span className="variable">id</span>).<span className="func">then</span>((<span className="variable">element</span>) => {'{}'})<br/>
-                <span className="variable">collection</span>.<span className="func">update</span>(<span className="variable">id</span>, <span className="variable">element</span>).<span className="func">then</span>((<span className="variable">success</span>) => {'{}'})<br/>
-                <span className="variable">collection</span>.<span className="func">delete</span>(<span className="variable">id</span>).<span className="func">then</span>((<span className="variable">success</span>) => {'{}'})<br/>
-            </p>
-        ),
-        'python': (
-            <p>
-                <span className="keyword">from </span><span className="variable">dbhub</span> <span className="keyword">import</span> <span className="func"></span><span className="string">get_database</span><br/>
-                <br/>
-                <span className="variable">api_key</span> =<span className="string"> '{apiKey}'</span><br/>
-                <span className="variable">db</span> =<span className="func"> get_database</span>(<span className="variable">api_key</span>)<br/>
-                <br/>
-                {'// Create collection'}<br/>
-                <span className="variable">collection_name</span> = <span className="string">'example'</span><br/>
-                <span className="variable">collection</span> = <span className="variable">db</span>.<span className="func">get_collection</span>(<span className="variable">collection_name</span>)<br/>
-                <br/>
-                {'//Usage'}<br/>
-                <span className="variable">collection</span>.<span className="func">create</span>(<span className="variable">element</span>)<br/>
-                <span className="variable">collection</span>.<span className="func">read</span>(<span className="variable">id</span>)<br/>
-                <span className="variable">collection</span>.<span className="func">update</span>(<span className="variable">id</span>, <span className="variable">element</span>)<br/>
-                <span className="variable">collection</span>.<span className="func">delete</span>(<span className="variable">id</span>)<br/>
-                <br/>
-            </p>
-        )}
-    return languageUsages[language]
+export const getUsageBlock = (language, token, dbName) => {
+    const code = {
+        'python':
+`from dbhub import get_database
+
+db = get_database(
+   '${token}',
+   '${dbName}'
+)
+
+collection = db.get_collection('users')
+collection[1] = {'name': 'Ivan'}
+user = collection[1]
+user.age = 5
+collection.update({1: {'age': user.age}})
+# user = {name: 'Ivan', age: user}
+
+del collection[1]`,
+
+        'javascript':
+            `const {getDatabase} = require('dbhub');
+
+const db = getDatabase(
+   '${token}',
+   '${dbName}'
+);
+
+const collection = db.getCollection('users');
+collection[1] = {name: 'Ivan'}
+let user = collection[1]
+user.age = 5
+collection.update({1: {age: user.age}})
+// user = {name: 'Ivan', age: user}
+
+collection.delete(1)`
+    }
+
+    return code[language]
 }
 
 export const getInstall = (language) => {
     let installs = {
-        'nodejs': 'npm install dbhub -s',
+        'javascript': 'npm install dbhub -s',
         'python': 'pip install dbhub'
     }
     return installs[language]
-}
-
-export const getUsage = (language, apikey) => {
-    const usage = {
-        'nodejs': ('const {getDatabase} = require(\'dbhub\')\n' +
-            '\n' +
-            'let apiKey = \'' + apikey + '\'\n' +
-            'let db = getDatabase(apiKey)\n' +
-            '\n' +
-            '// Create collection\n' +
-            'let collectionName = \'example\'\n' +
-            'let collection = db.getCollection(collectionName)\n' +
-            '\n' +
-            '//Usage\n' +
-            'collection.create(element).then((id) => {})\n' +
-            'collection.read(id).then((element) => {})\n' +
-            'collection.update(id, element).then((success) => {})\n' +
-            'collection.delete(id).then((success) => {})'),
-
-        'python':
-            ('from dbhub import get_database\n' +
-                '\n' +
-                'api_key = \'aa57dc3a-df8c-1c53-b5f7-6c598a7e4644\'\n' +
-                'db = get_database(api_key)\n' +
-                '\n' +
-                '# Create collection\n' +
-                'collection_name = \'example\'\n' +
-                'collection = db.get_collection(collection_name)\n' +
-                '\n' +
-                '# Usage\n' +
-                'collection.create(element)\n' +
-                'collection.read(id)\n' +
-                'collection.update(id, element)\n' +
-                'collection.delete(id)\n')
-    }
-    return usage[language]
 }
--- src/pages/Instructions/index.js
+++ src/pages/Instructions/index.js
@@ -4,9 +4,10 @@ import React, {useEffect, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 
 import {Toggle} from "../../components/ToggleButton"
-import {TextBlock} from "../../components/TextBlock"
-import {getInstall, getUsage, getUsageBlock, NODE_JS, PYTHON } from "./codeExample"
+import {getInstall, getUsageBlock, JAVASCRIPT, PYTHON } from "./codeExample"
 import {useAuth} from "../../network/auth"
+import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
+import { cb } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const instructionsClassName = 'instructions'
 const appear = 'appear'
@@ -23,7 +24,7 @@ function Instructions() {
     console.log(dbName)
 
     const languages = [
-        NODE_JS,
+        JAVASCRIPT,
         PYTHON,
     ]
 
@@ -39,11 +40,19 @@ function Instructions() {
         <div id={instructionsClassName}>
             <Toggle className='languageToggle' buttons={languages} cb={setToggledKey}/>
             <h3 className='label'>{t('your_api_key')}</h3>
-            <TextBlock className='apiKey' text={token} textToCopy={token}/>
+            <SyntaxHighlighter class="bordered" language='bash' style={cb}>
+                {token}
+            </SyntaxHighlighter>
             <h3 className='label'>{t('your_db_name')}</h3>
-            <TextBlock className='db-name' text={dbName} textToCopy={dbName}/>
-            <TextBlock className='install' text={'$ ' + getInstall(language)} textToCopy={getInstall(language)}/>
-            <TextBlock className='usage' text={getUsageBlock(language, token)} textToCopy={getUsage(language, token)}/>
+            <SyntaxHighlighter class="bordered" language='bash' style={cb}>
+                {dbName}
+            </SyntaxHighlighter>
+            <SyntaxHighlighter class="bordered" language='bash' style={cb}>
+                {'$ ' + getInstall(language)}
+            </SyntaxHighlighter>
+            <SyntaxHighlighter class="bordered" showLineNumbers={true} language={language} style={cb}>
+                {getUsageBlock(language, token, dbName)}
+            </SyntaxHighlighter>
         </div>
     )
 }
